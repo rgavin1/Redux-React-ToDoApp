7169d95c7006e4bbf5b99c2d20295726c582c0b5
rgavin1 <44215916+rgavin1@users.noreply.github.com>
2019-08-23 12:25:14 -0400

    Init Commit

---
+++ rr-react/src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import Todos from './Todos';
+
+class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1, 
+        content: 'buy some milk'
+      },
+      {
+        id: 2,
+        content: 'play mario kart'
+      }
+    ]
+  }
+  render(){
+    return (
+    <div className="App">
+      
+    </div>
+  );
+  }
+}
+
+export default App;
---
+++ rr-react/src/Todos.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Todos = () => {
+    return (
+        <div className="todos collection">
+
+        </div>
+    )
+}
+
+export default Todos;
